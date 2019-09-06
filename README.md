393f84f11888c1451e4f602da6859f009b962032
Anjan Roy <anjanroy@yandex.com>
2019-09-06 12:38:28 +0530

    added company class & category based classifier & plotted those data

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
 # corporateZ
 Data analysis done on Ministry of Corporate Affairs, Govt. of India's open data to get deeper insight, with :heart:
 
-### Coming soon ... :wink:
+## Analysis of M.C.A. Data for _West Bengal_
+---
+### Status of Companies
+Following PIE chart shows an overview of STATUS _( as of 21-04-2018 )_ of various companies in West Bengal, India.
+- Active ( 136423 )
+- Strike Off ( 48492 )
+- Amalgamated ( 5265 )
+- Under Process of Striking off ( 4485 )
+- Converted to LLP ( 2032 )
+- Converted to LLP and dissolved ( 867 )
+- Under liquidation ( 684 )
+- Not Available for eFiling ( 674 )
+- Dissolved ( 634 )
+- Dormant under section 455 ( 169 )
+- Liquidated ( 25 )
+- Active In Progress ( 2 )
+- Captured ( 1 )
+
+![status_of_companies_in_west_bengal](plots/mca_westbengal_21042018_company_status.png)
+
+---
+### Class of Companies
+Following PIE chart shows various companies categorized by their CLASS _( as of 21-04-2018 )_, present in West Bengal, India.
+- Private ( 179996 )
+- Public ( 19150 )
+- Private(One Person Company) ( 589 )
+- NA ( 17 )
+- PRIVATE ( 1 )
+
+![companies_categorized_by_class_in_west_bengal](plots/mca_westbengal_21042018_company_class.png)
+
+---
+### Category of Companies
+Following PIE chart shows various companies categorized by their CATEGORY _( as of 21-04-2018 )_, present in West Bengal, India.
+- Company limited by Shares ( 199056 )
+- Company Limited by Guarantee ( 647 )
+- Unlimited Company ( 31 )
+- NA ( 18 )
+- COMPANY LIMITED BY SHARES ( 1 )
+
+![companies_categorized_by_category_in_west_bengal](plots/mca_westbengal_21042018_company_category.png)
+
+---
+
+**More to come ...** :wink:
--- app.py
+++ app.py
@@ -2,31 +2,60 @@
 
 from re import compile as reg_compile
 from os.path import basename
+from functools import reduce
 try:
     from model.corporateStat import CompaniesUnderState
-    from util import categorizeAsPerCompanyStatus, plotCompanyStatusDataForAState
+    from util import categorizeAsPerCompanyStatus, categorizeAsPerCompanyClass, categorizeAsPerCompanyCategory, plotCategorizedCompanyDataForACertainState
 except ImportError as e:
     print('[!]Module Unavailable: {}'.format(str(e)))
     exit(1)
 
 
-def main(targetPath='./data/mca_westbengal_21042018.csv'):
-    def __extract_state__(fromIt: str):
+def main(targetPath='./data/mca_westbengal_21042018.csv') -> float:
+    '''
+        Given a CSV data file name, it'll simply extract
+        State name using Regular Expression(s)
+
+        In case of error, returns None
+    '''
+    def __extract_state__(fromIt: str) -> str:
         match_obj = reg_compile(r'^mca_(\w+)_[0-9]{8,}\.csv$').match(fromIt)
         return match_obj.group(1).capitalize() if match_obj else None
+
+    '''
+        A utility function to calculate division of 2-element tuple
+    '''
+    def __divide__(a: int, b: int) -> float:
+        return 0 if not b else a/b
+
+    '''
+        Given a iterable of boolean values, it'll return a 2-element tuple,
+        where first one will hold number of Truth values & last one will keep
+        total # of values we're considering
+    '''
+    def __calculateSuccess__(result):
+        return reduce(lambda acc, cur: (acc[0] + 1, acc[1]+1) if cur else (acc[0], acc[1]+1), result, (0, 0))
 
     try:
         companiesUnderStateObject = CompaniesUnderState.importFromCSV(
             __extract_state__(basename(targetPath)), targetPath=targetPath)
-        return plotCompanyStatusDataForAState(categorizeAsPerCompanyStatus(
-            companiesUnderStateObject.companies), './plots/{}_company_status.png'.format(basename(targetPath)[:-4]))
+        return __divide__(
+            *__calculateSuccess__([
+                plotCategorizedCompanyDataForACertainState(categorizeAsPerCompanyStatus(
+                    companiesUnderStateObject.companies), './plots/{}_company_status.png'.format(basename(targetPath)[:-4]), 'Status of Companies in West Bengal'),
+                plotCategorizedCompanyDataForACertainState(categorizeAsPerCompanyClass(
+                    companiesUnderStateObject.companies), './plots/{}_company_class.png'.format(basename(targetPath)[:-4]), 'Class of Companies in West Bengal'),
+                plotCategorizedCompanyDataForACertainState(categorizeAsPerCompanyCategory(
+                    companiesUnderStateObject.companies), './plots/{}_company_category.png'.format(basename(targetPath)[:-4]), 'Category of Companies in West Bengal')
+            ])
+        )  # calculating rate of success of these operation(s)
     except Exception:
-        return False
+        return 0.0
 
 
 if __name__ == '__main__':
     try:
-        print('Success' if main() else 'Failure')
+        print('Success: {} %'.format(main()*100))
     except KeyboardInterrupt:
         print('\n[!]Terminated')
     finally:
